29fc8a37f8f58dc2e1390f1cd51244d0e41de469
aa3268 <aa3268@github.com>
2015-02-13 16:47:18 -0500

    same as master

--- chess1/src/chess/ButtonBorder.java
+++ chess1/src/chess/ButtonBorder.java
@@ -1,6 +1,11 @@
+//Comment
+
 package chess;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Vector;
 
 import javax.swing.JButton;
@@ -8,59 +13,106 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
+@SuppressWarnings("serial")
 public class ButtonBorder extends JPanel {
-	
-	public ButtonBorder() {
+	  private static final int N = 8;
+	  private static final int SIZE = 75;
+      
+	  public ButtonBorder() {
+		    super(new GridLayout(N, N));
+		        this.setPreferredSize(new Dimension(N * SIZE, N * SIZE));
+		Position[][] p = new Position[8][8];
 		setLayout(new GridLayout(0, 8, 0, 0));
 		
-		JButton RB = new JButton("r");
+		Piece b;
+		//Position s;
+		
+		JButton RB = new JButton("2");
 		add(RB);
-		
-		JButton KnB = new JButton("kn");
+		b = new Piece();
+		b.button = RB;
+		b.color = "Black";
+		b.ident = "Rook";
+		//p[0][0] = b;
+		JButton KnB = new JButton("3");
 		add(KnB);
+		b = new Piece();
+		b.button = KnB;
+		b.color = "Black";
+		b.ident = "Knight";
+		//p[0][1] = b;
 		
-		JButton BB = new JButton("b");
+		JButton BB = new JButton("4");
 		add(BB);
+		b = new Piece();
+		b.button = BB;
+		b.color = "Black";
+		b.ident = "Bishop";
+		//p[0][2] = b;
 
-		JButton QB = new JButton("q");
+		JButton QB = new JButton("5");
 		add(QB);
+		b.button = QB;
+		b = new Piece();
+		b.color = "Black";
+		b.ident = "Queen";
+		//p[0][3] = b;
 		
-		JButton KB = new JButton("k");
+		JButton KB = new JButton("6");
 		add(KB);
+		b.button = KB;
+		b = new Piece();
+		b.color = "Black";
+		b.ident = "King";
+		//p[0][3] = b;
 		
-		JButton BB_1 = new JButton("b");
+		JButton BB_1 = new JButton("4");
 		add(BB_1);
+		b.button = BB_1;
+		b = new Piece();
+		b.color = "Black";
+		b.ident = "Bishop";
+		//p[0][5] = b;
 		
-		JButton KnB_1 = new JButton("kn");
+		JButton KnB_1 = new JButton("3");
 		add(KnB_1);
+		b = new Piece();
+		b.button = KnB_1;
+		b.color = "Black";
+		b.ident = "Knight";
+		//p[0][6] = b;
 		
-		
-		JButton RB_1 = new JButton("r");
+		JButton RB_1 = new JButton("2");
 		add(RB_1);
+		b = new Piece();
+		b.button = RB_1;
+		b.color = "Black";
+		b.ident = "Rook";
+		//p[0][7] = b;
 		
 		
-		JButton PB_1 = new JButton("p");
+		JButton PB_1 = new JButton("1");
 		add(PB_1);
 		
-		JButton PB_2 = new JButton("p");
+		JButton PB_2 = new JButton("1");
 		add(PB_2);
 	
-		JButton PB_3 = new JButton("p");
+		JButton PB_3 = new JButton("1");
 		add(PB_3);
 		
-		JButton PB_4 = new JButton("p");
+		JButton PB_4 = new JButton("1");
 		add(PB_4);
 		
-		JButton PB_5 = new JButton("p");
+		JButton PB_5 = new JButton("1");
 		add(PB_5);
 	
-		JButton PB_6 = new JButton("p");
+		JButton PB_6 = new JButton("1");
 		add(PB_6);
 	
-		JButton PB_7 = new JButton("p");
+		JButton PB_7 = new JButton("1");
 		add(PB_7);
 		
-		JButton PB_8 = new JButton("p");
+		JButton PB_8 = new JButton("1");
 		add(PB_8);
 	
 		
@@ -161,60 +213,168 @@ public class ButtonBorder extends JPanel {
 		
 		JButton PW_1 = new JButton("P");
 		add(PW_1);
+	
+		PW_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Piece b = new Piece();
+				b.button = PW_1;
+				b.color = "White";
+				b.ident = "Pawn";
+				
+				Position s = new Position();
+				s.y = 6;
+				s.x = 0;
+				s.p = b;
+				p[6][0] = s;
+				getPositions(s);
+				
+			}
+		});
 		
 		JButton PW_2 = new JButton("P");
 		add(PW_2);
+		b = new Piece();
+		b.button = PW_2;
+		b.color = "White";
+		b.ident = "Pawn";
+		//p[6][1] = b;
 		
 		JButton PW_3 = new JButton("P");
 		add(PW_3);
+		b = new Piece();
+		b.button = PW_3;
+		b.color = "White";
+		b.ident = "Pawn";
+		//p[6][2] = b;
 		
 		JButton PW_4 = new JButton("P");
 		add(PW_4);
+		b = new Piece();
+		b.button = PW_4;
+		b.color = "White";
+		b.ident = "Pawn";
+		//p[6][3] = b;
 		
 		JButton PW_5 = new JButton("P");
 		add(PW_5);
+		b = new Piece();
+		b.button = PW_5;
+		b.color = "White";
+		b.ident = "Pawn";
+		//p[6][4] = b;
 		
 		JButton PW_6 = new JButton("P");
 		add(PW_6);
+		b = new Piece();
+		b.button = PW_6;
+		b.color = "White";
+		b.ident = "Pawn";
+		//p[6][5] = b;
 		
 		JButton PW_7 = new JButton("P");
 		add(PW_7);
+		b = new Piece();
+		b.button = PW_7;
+		b.color = "White";
+		b.ident = "Pawn";
+		//p[6][6] = b;
 		
 		JButton PW_8 = new JButton("P");
 		add(PW_8);
+		b = new Piece();
+		b.button = PW_8;
+		b.color = "White";
+		b.ident = "Pawn";
+		//p[6][7] = b;
 		
-		JButton e111 = new JButton("R");
-		add(e111);
+		JButton RW = new JButton("R");
+		add(RW);
+		b = new Piece();
+		b.button = RW;
+		b.color = "White";
+		b.ident = "Rook";
+		//p[7][0] = b;
 		
-		JButton e112 = new JButton("Kn");
-		add(e112);
+		JButton KnW = new JButton("Kn");
+		KnW.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		add(KnW);	
+		KnW.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Piece b = new Piece();
+				b.button = KnW;
+				b.color = "White";
+				b.ident = "Knight";
+				
+				Position s = new Position();
+				s.y = 7;
+				s.x = 1;
+				s.p = b;
+				p[7][1] = s;
+			
+				getPositions(s);
+			}
+		});
 		
-		JButton e113 = new JButton("B");
-		add(e113);
 		
-		JButton e114 = new JButton("Q");
-		add(e114);
+		JButton BW = new JButton("B");
+		add(BW);
+		b = new Piece();
+		b.button = BW;
+		b.color = "White";
+		b.ident = "Bishop";
+		//p[7][2] = b;
 		
-		JButton e115 = new JButton("K");
-		add(e115);
+		JButton QW = new JButton("Q");
+		add(QW);
+		b = new Piece();
+		b.button = QW;
+		b.color = "White";
+		b.ident = "Queen";
+		//p[7][3] = b;
 		
-		JButton e116 = new JButton("B");
-		add(e116);
+		JButton KW = new JButton("K");
+		add(KW);
+		b = new Piece();
+		b.button = KW;
+		b.color = "White";
+		b.ident = "King";
+		//p[7][4] = b;
 		
-		JButton e117 = new JButton("Kn");
-		add(e117);
+		JButton BW_1 = new JButton("B");
+		add(BW_1);
+		b = new Piece();
+		b.button = BW_1;
+		b.color = "White";
+		b.ident = "Bishop";
+		//p[7][5] = b;
 		
-		JButton e118 = new JButton("R");
-		add(e118);
+		JButton KnW_1 = new JButton("Kn");
+		add(KnW_1);
+		b = new Piece();
+		b.button = KnW_1;
+		b.color = "White";
+		b.ident = "Knight";
+		//p[7][6] = b;
+		
+		JButton RW_1 = new JButton("R");
+		add(RW_1);
+		b = new Piece();
+		b.button = RW_1;
+		b.color = "White";
+		b.ident = "Rook";
+		//p[7][7] = b;
 		
 	
 	}
-
+/*
     private static final int N = 8;
     private static final int SIZE = 75;
     
     
-    /*
+    
     public ButtonBorder() {
         super(new GridLayout(N, N));
         this.setPreferredSize(new Dimension(N * SIZE, N * SIZE));
@@ -237,9 +397,8 @@ public class ButtonBorder extends JPanel {
 	        }
 	        different = !different;
         }
-    }*/
-
-
+    }
+*/
 
     private void display() {
         JFrame f = new JFrame("ButtonBorder");
@@ -255,56 +414,98 @@ public class ButtonBorder extends JPanel {
 
             @Override
             public void run() {
+            	/*ButtonBorder frame = new ButtonBorder();
+   
+				frame.setVisible(true);
+            	 JFrame f = new JFrame("ButtonBorder");
+                 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                 //f.getContentPane().add(this);
+                 f.pack();
+                 f.setLocationRelativeTo(null);
+                 f.setVisible(true);*/
                 new ButtonBorder().display();
             }
         });
     }
-    
-    
-    public void CheckMove(String i, int x, int y)
-    {
-    	Vector<int[][]> v;
-
-    	switch(i)
-    	{
-    	case("P"):
-    	{
-    		int y_1 = y - 1;
-    		
-    		int y_2 = y - 2;
-    	}
-    	break;
-    	case("B"):
-    	{
-    	
-    	}
-    	break;
-    	case("R"):
-    	{
-    	
-    	}
-    	break;
-    	case("Kn"):
-    	{
-    	
-    	}
-    	break;
-    	case("Q"):
-    	{
-    	
-    	}
-    	break;
-    	case("K"):
-    	{
-    	
-    	}
-    	break;
-    
-    	
-    	
-    	
-    	}
+   
+    /*
+    * When user clicks on a piece on the board getPositions() will be called
+    * 
+    * There will be a 2 dimensional array of positions for the buttons on the board and
+    * we'll know which ones to highlight and which ones to disable using
+    * Vector<Position> available attribute of a piece
+    * 
+    * Currently it only prints out the available positions of pawn and knight
+    */
+	public void getPositions(Position s)
+	{
+		Position spot;
+		s.p.available = new Vector<Position>();
 		
-    }
-    
+		//Added some basic first moves
+		switch(s.p.ident)
+		{
+		case("Pawn"):
+			if(s.y == 6)
+			{
+				spot = new Position();
+				spot.y = 4;
+				spot.x = s.x;
+				s.p.available.add(spot);
+				System.out.println(s.p.available.size());
+				spot = new Position();
+				spot.y = 5;
+				spot.x = s.x;
+				s.p.available.add(spot);
+				System.out.println(s.p.available.size());
+			}
+			else
+			{
+				spot = new Position();
+				spot.y = s.y - 1;
+				spot.x = s.x;
+				s.p.available.add(spot);
+			}
+	
+			break;
+		case("Rook"):
+			
+			break;
+		case("Knight"):
+			spot = new Position();
+			spot.y = s.y - 2;
+			spot.x = s.x - 1;
+			if(s.x >= 0 && s.y >= 0)
+			{
+				s.p.available.add(spot);
+			}
+			spot = new Position();
+			spot.y = s.y - 2;
+			spot.x = s.x + 1;
+			if(s.x >= 0 && s.y >= 0)
+			{
+				s.p.available.add(spot);
+			}
+			
+			break;
+		case("Bishop"):
+			
+			break;
+		case("Queen"):
+			
+			break;
+		case("King"):
+			
+			break;
+		
+		
+		}
+		for(int i = 0; i < s.p.available.size(); i++)
+		{
+			if(s.p.available.size() != 0)
+			{
+				System.out.println(s.p.available.get(i).x + " " + s.p.available.get(i).y);
+			}
+		}
+	}
 }
---
+++ chess1/src/chess/LogIn.java
@@ -0,0 +1,176 @@
+package chess;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+
+
+@SuppressWarnings("serial")
+public class LogIn extends JFrame {
+
+	private JPanel contentPane;
+	public final JTextField text;
+	public final JTextField text1;
+	public static LogIn frame;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new LogIn();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LogIn() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 260, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(UIManager.getColor("Panel.background"));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		text = new JTextField();
+		text.setBounds(108, 54, 86, 20);
+		contentPane.add(text);
+		text.setColumns(10);
+		text.setText("");
+		
+		text1 = new JTextField();
+		text1.setBounds(108, 85, 86, 20);
+		contentPane.add(text1);
+		text1.setColumns(10);
+		text1.setText("");
+		
+		JLabel lblNewLabel = new JLabel("Username");
+		lblNewLabel.setBounds(10, 57, 88, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		lblNewLabel_1.setBounds(10, 88, 88, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JButton newUserButton = new JButton("Create New User");
+		newUserButton.setBackground(SystemColor.menu);
+		newUserButton.setFont(new Font("Arial", Font.BOLD, 10));
+		newUserButton.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+		newUserButton.setBounds(52, 227, 140, 23);
+		newUserButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.setVisible(false);
+				JFrame newUser = new JFrame();
+				newUser.setBounds(100,100,300,200);
+				newUser.setVisible(true);
+			}
+		});
+		contentPane.add(newUserButton);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBackground(UIManager.getColor("Panel.background"));
+		textArea.setBounds(10, 144, 224, 72);
+		contentPane.add(textArea);
+		
+		JButton login = new JButton("Log In");
+		login.addActionListener(new ActionListener() {
+			Map<String,String> m = new HashMap<String, String>();
+			@SuppressWarnings("static-access")
+			public void actionPerformed(ActionEvent arg0) {
+				try{
+					URL oracle = new URL("https://www.cs.drexel.edu/~aa3268/userData");
+			        URLConnection yc = oracle.openConnection();
+			        BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+			        String inputLine = in.readLine();
+			        String[] tokens = null;
+			        String id;
+			        String password;
+			        
+			        while(inputLine != null)
+			        {
+				        //inputLine = in.readLine();
+			        	//System.out.println(inputLine);
+			        	tokens = inputLine.split(",");
+			        	id = tokens[0].trim();
+			        	password = tokens[3].trim();
+			        	m.put(id, password);
+			        	inputLine = in.readLine();
+			        }	
+			 
+					//System.out.println(m.toString());
+				in.close();
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				if(!text.getText().equals("") && !text1.getText().equals(""))
+				{
+					if(m.get(text.getText()).equals(text1.getText()))
+					{
+						MainMenu k;
+						try {
+							k = new MainMenu(text.getText());
+							String[] args = null;
+							k.main(args, text.getText());
+							frame.dispose();
+						} catch(Exception e){
+								e.printStackTrace();
+							}
+					}
+					else
+					{
+						
+						System.out.println(m.get(text.getText()));
+							
+					}
+				}
+				else if(text.getText().equals("") && !text1.getText().equals(""))
+				{
+					textArea.setText("Pleaser Enter Username");
+				}
+				else if(text1.getText().equals("") && !text.getText().equals(""))
+				{
+					textArea.setText("Pleaser Enter Password");
+				}
+				else
+				{
+					textArea.setText("Pleaser Enter Username and Password");
+				}
+					
+					
+					
+				}
+			
+				
+		});
+		login.setBounds(71, 117, 92, 23);
+		contentPane.add(login);
+
+	}
+}
